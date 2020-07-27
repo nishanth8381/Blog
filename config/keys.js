65846b41e7bb681d0a9c39600bcc7bae4df5e21b
Nishanth <nishanth.nuthakki@gmail.com>
2020-07-27 13:34:48 +0530

    setting up database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+MONGOURI:"mongodb+srv://nishanth2000:hanumann@cluster0.i4n6t.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
+const mongoose = require('mongoose')
+
+mongoose.connect(MONGOURI,{useUnifiedTopology:true,useNewUrlParser:true})
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to mongoDB!!');
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log(err);
+})
 
 app.listen(PORT,()=>{
     console.log('server is running!!')
